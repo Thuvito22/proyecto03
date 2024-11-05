97041d9b4ebfa9cdc47b4dd503a06a07a50a311f
grupo01 <notengocorreo@notengo.com>
2024-11-05 13:59:08 +0100

    Calculadora pendiente de hacer test

---
+++ src/main/java/es/santander/ascender/proyecto03/App.java
@@ -0,0 +1,22 @@
+package es.santander.ascender.proyecto03;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World! Estamos escribiendo");
+   
+   
+        Calculadora calculadora = new Calculadora();
+        long resultado = calculadora.sumar(5, 4);
+        System.out.println(resultado);
+   
+   
+    }
+
+
+
+
+
+}
