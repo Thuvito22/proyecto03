619ffa720ae65b0e5225dff30098e38b7091c570
grupo01 <notengocorreo@notengo.com>
2024-11-06 14:00:54 +0100

    Empezando Arrays

--- src/main/java/es/santander/ascender/proyecto03/Calculadora.java
+++ src/main/java/es/santander/ascender/proyecto03/Calculadora.java
@@ -16,4 +16,32 @@ public class Calculadora {
         return dividendo / divisor;
     }
 
+
+    public void presentaTablaMultiplicar(int numero) {
+        for (int i = 0 ; i <= 10; i = i + 1) {
+
+            System.out.println(i + numero);
+
+        } 
+    
+    }
+
+    public void calcularTablamultiplicarprense(int numero){
+        int[] resultados = null; resultados = null;
+
+        int [] borrame = new int [11];
+        borrame [0] = 3;
+
+        borrame [4] = 6;
+
+        borrame [7] = borrame[0] + borrame[4];
+
+        borrame [7] = borrame [0] + borrame [4];
+        System.out.println(borrame[7]);
+
+
+
+    }
 }
+
+
--- src/test/java/es/santander/ascender/proyecto03/CalculadoraTest.java
+++ src/test/java/es/santander/ascender/proyecto03/CalculadoraTest.java
@@ -4,13 +4,35 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculadoraTest {
+    Calculadora calculadora = new Calculadora();
 
     @Test
     public void testSumar() {
-        Calculadora calculadora = new Calculadora();
 
         long resultado = calculadora.sumar(5, 10);
-        
+
         assertEquals(15, resultado, "10+5 debería ser 15");
+    }
+
+    @Test
+    void testRestar() {
+        long resultado = calculadora.restar(10, 4);
+        assertEquals(6, resultado, "10-4 esperaba 6");
+    }
+
+    @Test
+    public void testPresentaTablaMultiplicar() {
+        calculadora.presentaTablaMultiplicar(8); 
+    }
+
+
+    @Test
+    public void testCalcularTablaMultiplicar () {
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.calcularTablamultiplicar(9);
+        
+
+
     }
 }
